8788a4a8d363ef3702358f8361387a5904a862d3
blacksonic <soos.gabor86@gmail.com>
2019-11-22 12:56:35 +0100

    feat(logger): add client logger

---
+++ src/logger.js
@@ -0,0 +1,4 @@
+import { initialize } from '@emartech/client-logger';
+
+const LoggerFactory = initialize('vue-example');
+export const loggerFactory = (name) => LoggerFactory.create(name);
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import App from './components/app.vue';
 import { storeFactory } from './store';
+import { loggerFactory } from './logger';
 
 Vue.use(Vuex);
 Vue.config.productionTip = false;
+const logger = loggerFactory('app');
 
 storeFactory(window.e)
   .then(store => new Vue({ store, render: h => h(App) }).$mount('#app'))
-  .then(() => console.log('Application vue-example started'))
-  .catch(error => console.error('Application vue-example crashed', error));
+  .then(() => logger.info('start'))
+  .catch(error => logger.fromError('start', error));
