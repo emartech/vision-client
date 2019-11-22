ae58eeab15e1262464ee37c2796c8d4911f8a961
blacksonic <soos.gabor86@gmail.com>
2019-11-22 11:27:39 +0100

    feat(package): let console

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ export const storeFactory = emarsys => {
   return Promise
     .all([
       emarsys.utils.getConfig(),
-      emarsys.utils.getAuthenticationToken('vue-example')
+      emarsys.utils.getAuthenticationToken('example-service')
     ])
     .then(([config, authToken]) => {
       const state = { ...baseState, ...config, authToken };
